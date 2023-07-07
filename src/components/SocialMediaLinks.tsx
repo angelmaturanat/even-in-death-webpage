8ed3d44596ae6f4885a982e7b42485d9a134533a
Angel Maturana <angelmaturana@MacBook-Pro-de-Angel.local>
2023-07-07 13:31:03 -0400

    feature(styles): add new links and separate it by columns

--- src/components/SocialMediaLinks.tsx
+++ src/components/SocialMediaLinks.tsx
@@ -1,21 +1,30 @@
 import React from 'react';
-import { faFacebookF, faTwitter, faInstagram, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { faFacebookF, faTwitter, faInstagram, faWhatsapp, faSpotify, faApple } from '@fortawesome/free-brands-svg-icons';
 import SocialMediaItem from './SocialMediaItem';
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 
 const SocialMediaLinks: React.FC = () => {
   return (
-    <div>
-      <h1 className="mb-2 text-2xl">Nuestras Redes Sociales</h1>
-      <ul className="divide-y center">
-        <SocialMediaItem icon={faCartShopping} url={'https://www.puntoticket.com/milenia-2023'} text={'¡Venta de entradas! Evanescence en Chile'}></SocialMediaItem>
-        <SocialMediaItem icon={faFacebookF} url={'https://www.facebook.com/evanescencechileoficial'} text={'Facebook'}></SocialMediaItem>
-        <SocialMediaItem icon={faTwitter} url={'https://twitter.com/Evan_Chile'} text={'Twitter'}></SocialMediaItem>
-        <SocialMediaItem icon={faInstagram} url={'https://www.instagram.com/evenindeathchile/'} text={'Instagram'}></SocialMediaItem>
-        <SocialMediaItem icon={faWhatsapp} url={'https://chat.whatsapp.com/L4NnVKNemE3FnVbV5kqoFv'} text={'Whatsapp Chile'}></SocialMediaItem>
-        <SocialMediaItem icon={faWhatsapp} url={'https://chat.whatsapp.com/BoU1S1Op03qEyXspghGzqq'} text={'Whatsapp Concepción'}></SocialMediaItem>
-        <SocialMediaItem icon={faWhatsapp} url={'https://chat.whatsapp.com/CtuzAz5sTdh6dff2xOEM8M'} text={'Whatsapp La Serena'}></SocialMediaItem>
-      </ul>
+    <div className='grid grid-cols-2 gap-4'>
+      <div>
+        <h1 className="mb-2 text-2xl text-center">Enlaces Importantes</h1>
+        <ul className="divide-y center">
+          <SocialMediaItem icon={faCartShopping} url={'https://www.puntoticket.com/milenia-2023'} text={'¡Venta de entradas! Evanescence en Chile'}></SocialMediaItem>
+          <SocialMediaItem icon={faApple} url={'https://music.apple.com/cl/playlist/evanescence-en-chile-2023-milenia-fest/pl.u-xlJXTGaYRpr'} text={'Apple Music Milenia Playlist'}></SocialMediaItem>
+          <SocialMediaItem icon={faSpotify} url={'https://open.spotify.com/playlist/65zKEUX6WW6I03Cnbd13Ql?si=8-dO6P2VT4axcsTJews7dg'} text={'Spotify Milenia Playlist'}></SocialMediaItem>
+        </ul>
+      </div>
+      <div>
+        <h1 className="mb-2 text-2xl text-center">Nuestras Redes Sociales</h1>
+        <ul className="divide-y center">
+          <SocialMediaItem icon={faFacebookF} url={'https://www.facebook.com/evanescencechileoficial'} text={'Facebook'}></SocialMediaItem>
+          <SocialMediaItem icon={faTwitter} url={'https://twitter.com/Evan_Chile'} text={'Twitter'}></SocialMediaItem>
+          <SocialMediaItem icon={faInstagram} url={'https://www.instagram.com/evenindeathchile/'} text={'Instagram'}></SocialMediaItem>
+          <SocialMediaItem icon={faWhatsapp} url={'https://chat.whatsapp.com/L4NnVKNemE3FnVbV5kqoFv'} text={'Whatsapp Chile'}></SocialMediaItem>
+          <SocialMediaItem icon={faWhatsapp} url={'https://chat.whatsapp.com/BoU1S1Op03qEyXspghGzqq'} text={'Whatsapp Concepción'}></SocialMediaItem>
+          <SocialMediaItem icon={faWhatsapp} url={'https://chat.whatsapp.com/CtuzAz5sTdh6dff2xOEM8M'} text={'Whatsapp La Serena'}></SocialMediaItem>
+        </ul>
+      </div>
     </div>
   );
 };
